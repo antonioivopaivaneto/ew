d34bfeb6aeffc360370c365025d8589921f7777d
junior <antonioivo.3@gmail.com>
2024-01-04 10:01:33 -0300

    events

--- 02-node/index.js
+++ 02-node/index.js
@@ -48,8 +48,16 @@ async function main() {
   try {
     console.time('medida-promise')
     const usuario = await obterUsuario();
-    const telefone = await obterTelefone(usuario.id);
-    const endereco = await obterEnderesoAsync(usuario.id);
+    //const telefone = await obterTelefone(usuario.id);
+    //const endereco = await obterEnderesoAsync(usuario.id);
+
+    const resultado = await Promise.all([
+        obterTelefone(usuario.id),
+        obterEnderesoAsync(usuario.id)
+    ])
+
+    const endereco = resultado[1]
+    const telefone = resultado[0]
 
     console.log(`
         Nome ${usuario.nome}
